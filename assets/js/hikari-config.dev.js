c875dfa12bf6d33837f1629d273eb8053dcbbc4b
t.k <ceo@air-admin8.co.jp>
2026-08-22 20:04:27 +0900

    chore: connect HIKARI DEV widget to secure Vercel proxy

--- assets/js/hikari-config.dev.js
+++ assets/js/hikari-config.dev.js
@@ -1,5 +1,5 @@
 window.HIKARI_CONFIG = {
   environment: 'DEV/UAT',
-  endpoint: '',
+  endpoint: 'https://hikari-proxy-dev-git-dev-ceo-2852s-projects.vercel.app/api/hikari',
   appName: 'HIKARI_DEV/UAT'
 };
